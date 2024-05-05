555f44f98e42aa9b5ec50d4ed96471131b82bd4b
Gonçalo Branquinho <goncalobranquinhom@gmail.com>
2024-05-05 20:06:32 +0100

    Update game.py

--- Chess/game.py
+++ Chess/game.py
@@ -7,64 +7,63 @@ import time
 
  
 class Game:
-    def __init__(self) -> None:
-        self.player = Player("Undefined")
-        self.board = Chessboard()
-        self.logic = Logic("None")
-        self.opositeColor = {"White": "Black",
-                             "Black": "White"}
-        self.main()
-    
-    def main(self):
+ def main():
         # initialize the game
-        self.board.initialize()
-        self.board.draw()
+        player = Player("Undefined")
+        board = Chessboard()
+        logic = Logic("None")
+        opositeColor = {"White": "Black",
+                        "Black": "White"}
+        board.initialize()
+        board.draw()
         turn = 2
         while (True):
-            # time.sleep(0.5)
+            time.sleep(0.5)
 
             if turn % 2 == 0:
-                self.player.color = "White"
+                player.color = "White"
             else:
-                self.player.color = "Black"
+                player.color = "Black"
             
-            self.logic.toMoveColor = self.player.color
-            self.logic.moves = []
-            self.logic.valid_moves = []
-            self.logic.table = self.board.table
+            logic.toMoveColor = player.color
+            logic.moves = []
+            logic.valid_moves = []
+            logic.table = board.table
             
-            if self.logic.insufficientMaterial() or self.logic.drawnByRepetition():
+            if logic.insufficientMaterial() or logic.drawnByRepetition():
                 print("  The game ends in a draw")
                 break
             
-            self.logic.generate_moves()        
+            logic.generate_moves()        
             
-            if self.logic.game_over():
+            if logic.game_over():
                 break
             
             else:
-                if self.logic.kingInCheck():
-                    self.logic.give_hint()
+                
+                if logic.kingInCheck():
+                    logic.give_hint()
                 while(True):
-                    # (self.logic.x1, self.logic.y1, self.logic.x2, self.logic.y2) = random.choice(self.logic.valid_moves) # Uncomment this line to get random choices
-                    self.logic.x1, self.logic.y1, self.logic.x2, self.logic.y2 = self.player.get_input(self.board.table) # Comment this line to get random choices
-                    self.logic.updateCastling()
-                    self.logic.printInfo()
-                    x1, y1, x2, y2 = self.logic.x1, self.logic.y1, self.logic.x2, self.logic.y2
+                    
+                    # (logic.x1, logic.y1, logic.x2, logic.y2) = random.choice(logic.valid_moves) # Uncomment this line to get random choices
+                    logic.x1, logic.y1, logic.x2, logic.y2 = player.get_input(board.table) # Comment this line to get random choices
+                    logic.updateCastling()
+                    logic.printInfo()
+                    x1, y1, x2, y2 = logic.x1, logic.y1, logic.x2, logic.y2
 
-                    if (x1,y1,x2,y2) in self.logic.valid_moves:
-                            self.logic.trackPreviousMove[self.opositeColor[self.player.color]] = "None"
-                            self.logic.updatePieceMovements()
-                            moved = self.board.table[x1][y1]
+                    if (x1,y1,x2,y2) in logic.valid_moves:
+                            logic.trackPreviousMove[opositeColor[player.color]] = "None"
+                            logic.updatePieceMovements()
+                            moved = board.table[x1][y1]
                             if (x2 == MAX_HEIGHT and moved == B_PAWN) or (x2 == MIN_HEIGHT and moved == W_PAWN):
-                                self.logic.pawn_reach_end()
+                                logic.pawn_reach_end()
                             else:    
-                                self.board.table[x2][y2] = moved
-                                self.board.table[x1][y1] = " "
-                            self.board.draw()
+                                board.table[x2][y2] = moved
+                                board.table[x1][y1] = " "
+                            board.draw()
                             turn += 1 
                             break
                     else:
                         print("  Invalid Move, play again\n")
 
-Game()
+    main()
